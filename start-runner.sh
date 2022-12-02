e4c380c20d830b6b964ec195ab49b57b42594ff9
Rian <riankatoen@gmail.com>
2022-12-02 20:14:00 +0100

    docker-image.yml - change hostname for the image

--- start-runner.sh
+++ start-runner.sh
@@ -4,7 +4,7 @@ if [[ ! -e $CONTAINER_INITIALIZED ]]
 then
     echo "Starting first time initialization..."
     echo $GITHUB_URL
-    echo $GITHUB_TOKEN
+    echo $GITHUB_TOKEN  
     ./config.sh --unattended --url $GITHUB_URL --token $GITHUB_TOKEN
     touch $CONTAINER_INITIALIZED
     echo "Completed first time initialization!"
